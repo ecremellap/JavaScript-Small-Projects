1561985bac78dd6c18650ffa0cb91dcc9d53f2ff
ecremellap <ejc6310@mavs.uta.edu>
2026-07-29 13:53:45 -0500

    Variables

---
+++ Basic/01-variables.js
@@ -0,0 +1,16 @@
+console.log(hello);
+var hello = "Hello";
+console.log(hello);
+hello = "Hello again and again";
+console.log(hello);
+
+//console.log(hello2);
+let hello2 = "hello 2";
+console.log(hello2);
+hello2 = "hello 2 again and again";
+console.log(hello2);
+
+const hello3 = "hello 3";
+console.log(hello3);
+/*hello3 = "changed";
+console.log(hello3);*/
