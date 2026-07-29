4a0e2a2c0079363014e1ffcaf520c42ab7d96398
ecremellap <ejc6310@mavs.uta.edu>
2026-07-29 14:18:31 -0500

    Data Types - Showing data types after printing them

---
+++ Basic/02-dataTypes.js
@@ -0,0 +1,42 @@
+//Strings
+let string1 = "hello";
+let string2 = 'hello';
+let string3 = `hello`;
+
+//Integer
+let age = 90;
+//Float
+let height = 1.70;
+
+//Boolean
+let isTrue = true;
+let isFalse = false; 
+
+//Undefined 
+let undefinedValue;
+//console.log(undefined);
+
+//Null
+let nullValue = null;
+//console.log(nullValue);
+
+//Symbol
+let mySymbol = Symbol("mysymbol");
+
+//Big Integer
+let myBigInt = BigInt(8984948494894949484891895161561616516161616151);
+let myBigInt2 = BigInt(8984948494894949484891895161561616516161616152);
+
+//Showing data types
+console.log(typeof height);
+console.log(typeof age);
+console.log(typeof string1);
+console.log(typeof string2);
+console.log(typeof string3);
+console.log(typeof isTrue);
+console.log(typeof isFalse);
+console.log(typeof undefinedValue);
+console.log(typeof nullValue);
+console.log(typeof mySymbol);
+console.log(typeof myBigInt);
+console.log(typeof myBigInt2);
