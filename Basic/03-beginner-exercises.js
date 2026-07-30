aa0a5029a940c7323b0f009a78dfeecb54058178
ecremellap <ejc6310@mavs.uta.edu>
2026-07-29 19:29:58 -0500

    Excercises to warm up JS syntax and data types

---
+++ Basic/03-beginner-exercises.js
@@ -0,0 +1,101 @@
+// 1. Escribe un comentario en una linea
+// THIS IS THE COMMENT
+
+// 2. Escribe un comentario en varias lineas
+/*  Comentario en
+    varias lineas
+*/
+
+// 3. Declara varaibles con valores asociaos a todos los datos de tipo primitivos
+let height = 2.5;
+let age = 98;
+let string = "hi there!";
+let isTrue = true;
+let isFalse = false;
+let undefinedVal;
+let nullVal = null;
+let mySymbol = Symbol("My Symbol");
+let myBigInt = BigInt(25166168116816818656516161561616516161616516);
+
+// 4. Imprime por consola el valor de todas las variables
+console.log(height);
+console.log(age);
+console.log(string);
+console.log(isTrue);
+console.log(isFalse);
+console.log(undefinedVal);
+console.log(nullVal);
+console.log(mySymbol);
+console.log(myBigInt);
+
+// 5. Imprime por consola el tipo de todas las variables 
+console.log(typeof height);
+console.log(typeof age);
+console.log(typeof string);
+console.log(typeof isTrue);
+console.log(typeof isFalse);
+console.log(typeof undefinedVal);
+console.log(typeof nullVal);
+console.log(typeof mySymbol);
+console.log(typeof myBigInt);
+
+// 6. Modifica valores de las variables por otros del mismo tipo
+height = 5.76
+console.log(height);
+age = 84;
+console.log(age);
+string = "wait a minute";
+console.log(string);
+isTrue = false;
+console.log(isTrue);
+isFalse = true;
+console.log(isFalse);
+let undif;
+undefinedVal = undif;
+console.log(undefinedVal);
+nullVal2 = null;
+nullVal = nullVal2
+console.log(nullVal);
+mySymbol = Symbol("now, this is the new symbol");
+console.log(mySymbol);
+myBigInt = BigInt(618161176181987191781887787878878787878878778788787877878878789999);
+console.log(myBigInt);
+
+// 7. Modifica los valores de la variables por otros de distinto tipo
+height = "WHAT IS THIS?!"
+console.log(height);
+age = Symbol("An age?!");
+console.log(age);
+string = 52;
+console.log(string);
+isTrue = BigInt(777777777777777777777777777777777777777777777777777777777777777777777);
+console.log(isTrue);
+isFalse = 6.45481;
+console.log(isFalse);
+undefinedVal = null;
+console.log(undefinedVal);
+console.log(nullVal);
+mySymbol = true;
+console.log(mySymbol);
+myBigInt = false;
+console.log(myBigInt);
+
+// 8. Declara constantes con valores asociados a todos los tipos de datos primitivos
+const weight2 = 5.76;
+console.log(weight2);
+const number2 = 10;
+console.log(number2);
+const string4 = "wait a minute";
+console.log(string4);
+const isTrue2 = false;
+console.log(isTrue2);
+const isFalse2 = true;
+console.log(isFalse2);
+//const undif2;
+//console.log(undif2);
+const nullVal3 = null;
+console.log(nullVal3);
+const mySymbol2 = Symbol("now, this is the new symbol");
+console.log(mySymbol2);
+const myBigInt2 = BigInt(618161176181987191781887787878878787878878778788787877878878789999);
+console.log(myBigInt2);
